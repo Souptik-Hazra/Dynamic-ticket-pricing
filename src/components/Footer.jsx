991e1d627059efe83c26be580447cce6b365e96a
Souptik Hazra <116510045+Souptik-Hazra@users.noreply.github.com>
2026-02-24 14:45:33 +0530

    v3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import './Footer.css';
 
 const Footer = () => (
   <footer className="footer">
     <div className="footer-content">
-      <span>© 2026 Dynamic Ticket Pricing. All rights reserved.</span>
+      <span>Name: Souptik Hazra</span>&nbsp;&nbsp;&nbsp;
+      <span>Profession: Student</span>&nbsp;&nbsp;&nbsp;&nbsp;
+      <span>This is a full stack project using MERN.</span>
     </div>
   </footer>
 );
